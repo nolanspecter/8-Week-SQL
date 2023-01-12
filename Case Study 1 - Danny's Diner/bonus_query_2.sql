c04fc10e4a72838add721e3e85a36e24c4d85b7b
Duc Dang <anhduc031100@gmail.com>
2023-01-12 15:11:03 +0000

    Finished Week 1

---
+++ Case Study 1 - Danny's Diner/bonus_query_2.sql
@@ -0,0 +1,23 @@
+WITH temp AS(
+  SELECT s.customer_id, s.order_date, m.product_name, m.price, 
+          CASE 
+              WHEN s.customer_id IN (SELECT customer_id FROM dannys_diner.members)
+                  AND s.order_date >= c.join_date
+                  THEN 'Y'
+              ELSE 'N'
+           END AS member
+  FROM dannys_diner.sales AS s
+  JOIN dannys_diner.menu AS m
+  ON s.product_id = m.product_id
+  LEFT JOIN dannys_diner.members AS c
+  ON s.customer_id = c.customer_id
+  ORDER BY s.customer_id, s.order_date, m.price DESC
+)
+
+SELECT *,
+		CASE
+		WHEN member = 'Y' 
+		THEN RANK() OVER (PARTITION BY customer_id, member ORDER BY order_date)
+		ELSE NULL
+		END AS ranking
+FROM temp;
