bb7e78405f83c22348adfd851a785975225cc032
Duc Dang <anhduc031100@gmail.com>
2023-01-31 17:29:56 +0000

    Refactor E&C Exp Code

--- Case Study 2 - Pizza Runner/Runner and Customer Experience/query_2.sql
+++ Case Study 2 - Pizza Runner/Runner and Customer Experience/query_2.sql
@@ -5,7 +5,7 @@ WHERE pickup_time = 'null';
 ALTER TABLE pizza_runner.runner_orders
 ALTER COLUMN pickup_time TYPE TIMESTAMP USING CAST(pickup_time AS TIMESTAMP) ;
 
-SELECT R.runner_id, AVG(O.order_time - R.pickup_time)
+SELECT R.runner_id, AVG(R.pickup_time - O.order_time)
 FROM pizza_runner.customer_orders AS O
 JOIN pizza_runner.runner_orders AS R
 ON O.order_id = R.order_id
--- Case Study 2 - Pizza Runner/Runner and Customer Experience/query_3.sql
+++ Case Study 2 - Pizza Runner/Runner and Customer Experience/query_3.sql
@@ -5,7 +5,7 @@ WHERE pickup_time = 'null';
 ALTER TABLE pizza_runner.runner_orders
 ALTER COLUMN pickup_time TYPE TIMESTAMP USING CAST(pickup_time AS TIMESTAMP) ;
 
-SELECT O.order_id, COUNT(O.pizza_id) AS num_of_pizzas, AVG(O.order_time - R.pickup_time)  AS prep_time
+SELECT O.order_id, COUNT(O.pizza_id) AS num_of_pizzas, AVG(R.pickup_time - O.order_time)  AS prep_time
 FROM pizza_runner.customer_orders AS O
 JOIN pizza_runner.runner_orders AS R
 ON O.order_id = R.order_id
